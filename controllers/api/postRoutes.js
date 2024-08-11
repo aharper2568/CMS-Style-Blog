12b48a24410418b7a8742f8de4b99fdbeac4790c
aharper2568 <anthonyharper568@gmail.com>
2024-08-11 17:53:08 -0400

    added CRUD options to postRoutes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,96 @@
+const router = require('express').Router();
+const { User, Post } = require('../../models');
+
+const withAuth = require('../../utils/auth')
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+    const post = postData.get({plain: true});
+    res.render('single-post', {
+      post,
+      logged_in: req.session.logged_in,
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.post('/', withAuth, async (req, res) => {
+  try {
+      const newPost = await Post.create({
+          title: req.body.title,
+          content: req.body.content,
+          user_id: req.session.user_id,
+      });
+
+      res.status(200).json(newPost);
+  } catch (err) {
+      res.status(400).json(err);
+  }
+});
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedPost = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!updatedPost) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.delete('/:id', async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
