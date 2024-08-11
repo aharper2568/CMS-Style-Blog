c7e0a2f61039e69c9bd29264f85f1961d580b1d7
aharper2568 <anthonyharper568@gmail.com>
2024-08-11 19:28:59 -0400

    added POST fetch request and page reload on commentary in public js

---
+++ public/js/single-post.js
@@ -0,0 +1,23 @@
+// public/js/single-post.js
+
+
+document.querySelector('#new-comment-form').addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const content = document.querySelector('#comment-content').value.trim();
+  const postId = window.location.pathname.split('/').pop();
+
+  if (content) {
+    const response = await fetch(`/post/${postId}/comment`, {
+      method: 'POST',
+      body: JSON.stringify({ content }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert('Failed to add comment');
+    }
+  }
+});
