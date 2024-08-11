6feca2227be13749af8c4767eaa2e4c43782f607
aharper2568 <anthonyharper568@gmail.com>
2024-08-11 17:49:40 -0400

    updated homeRoutes for blog setting, added signup function

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,25 +1,75 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    const postData = await Post.findAll({
+      include:[{model: User}]
     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
+
 
     res.render('homepage', {
-      users,
+      posts,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+// Route to render the new post page
+router.get('/new-post', withAuth, (req, res) => {
+  res.render('new-post', {
+      logged_in: req.session.logged_in
+  });
+});
 
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id,{
+      include:[{model: User}]
+    });
+
+    const posts = postData.get({ plain: true });
+
+
+    res.render('single-post', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// Route to render the new post page
+router.get('/new-post', withAuth, (req, res) => {
+  res.render('new-post', {
+      logged_in: req.session.logged_in
+  });
+});
+
+
+
+// router.get('/newpost', withAuth, async (req, res) => {
+//   try {
+// res.render('newpost', {
+//   loggedIn: req.session.loggedIn
+// })
+//   } catch (err) {
+//     res.status(500).json(err)
+//   }
+// })
+
+router.get('/register', (req, res) => {
+  if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+  }
+
+  res.render('register');
+});
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
@@ -28,5 +78,6 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+
 
 module.exports = router;
