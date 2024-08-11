5b02218d94e37f65836d40221a7e7d48d45b7664
aharper2568 <anthonyharper568@gmail.com>
2024-08-11 17:57:02 -0400

    pushing homepage.js for tracking purposes, needs updated

---
+++ public/js/homepage.js
@@ -0,0 +1,6 @@
+// const makePostBtn = document.querySelector('.newPostBtn')
+// const newPostHandler = async (event) => {
+//   event.preventDefault();
+//  await document.location.replace('/new-post')
+// }
+// makePostBtn.addEventListener('click', newPostHandler)
